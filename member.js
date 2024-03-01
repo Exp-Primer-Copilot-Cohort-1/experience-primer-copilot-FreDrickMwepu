8478944701b50b3cf7ec005da0522f4ccaa82564
Fredrick Mwepu <fredrickmwepu22@gmail.com>
2024-03-01 04:23:41 +0200

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,107 @@
+function skillsMember() {
+  return {
+    "id": "FreDrickMwepu",
+    "name": "FreDrick Mwepu",
+    "skills": [
+      {
+        "name": "JavaScript",
+        "level": "advanced"
+      },
+      {
+        "name": "React",
+        "level": "advanced"
+      },
+      {
+        "name": "Node.js",
+        "level": "advanced"
+      },
+      {
+        "name": "Express",
+        "level": "advanced"
+      },
+      {
+        "name": "MongoDB",
+        "level": "advanced"
+      },
+      {
+        "name": "SQL",
+        "level": "advanced"
+      },
+      {
+        "name": "HTML",
+        "level": "advanced"
+      },
+      {
+        "name": "CSS",
+        "level": "advanced"
+      },
+      {
+        "name": "Python",
+        "level": "advanced"
+      },
+      {
+        "name": "Django",
+        "level": "advanced"
+      },
+      {
+        "name": "Flask",
+        "level": "advanced"
+      },
+      {
+        "name": "C#",
+        "level": "advanced"
+      },
+      {
+        "name": "Java",
+        "level": "advanced"
+      },
+      {
+        "name": "Spring",
+        "level": "advanced"
+      },
+      {
+        "name": "C++",
+        "level": "advanced"
+      },
+      {
+        "name": "C",
+        "level": "advanced"
+      },
+      {
+        "name": "PHP",
+        "level": "advanced"
+      },
+      {
+        "name": "Laravel",
+        "level": "advanced"
+      },
+      {
+        "name": "Ruby",
+        "level": "advanced"
+      },
+      {
+        "name": "Rails",
+        "level": "advanced"
+      },
+      {
+        "name": "Go",
+        "level": "advanced"
+      },
+      {
+        "name": "Rust",
+        "level": "advanced"
+      },
+      {
+        "name": "Kotlin",
+        "level": "advanced"
+      },
+      {
+        "name": "Swift",
+        "level": "advanced"
+      },
+      {
+        "name": "Objective-C",
+        "level": "advanced"
+      },
+      {
+        "
